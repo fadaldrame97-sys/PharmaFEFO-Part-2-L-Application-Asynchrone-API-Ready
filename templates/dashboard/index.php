f18e35a9bffc9ca6b3b63f87f9aee314294041e9
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-06-16 16:54:10 +0100

    Ajouter le Html brute sans données et préparer le js

---
+++ templates/dashboard/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- HTML seulement -->
+<body>
+
+<h1>PharmaFEFO</h1>
+
+<div id="stats"></div>
+
+<table>
+  <thead>
+    <tr>
+      <th>Produit</th>
+      <th>Quantité</th>
+      <th>Expiration</th>
+      <th>Action</th>
+    </tr>
+  </thead>
+
+  <tbody id="batch-table">
+   
+  </tbody>
+</table>
+
+<script src="/js/dashboard.js"></script>
+
+</body>
+</body>
+</html>
