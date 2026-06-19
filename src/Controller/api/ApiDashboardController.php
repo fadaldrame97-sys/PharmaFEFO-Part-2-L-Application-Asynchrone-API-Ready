d0071417c72c8a83c59abdc817c118a5d8ce3a0c
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-06-19 10:11:05 +0100

    créer class ApiDashboardController et l'instancier dans le contructeur

--- src/Controller/api/ApiDashboardController.php
+++ src/Controller/api/ApiDashboardController.php
@@ -2,3 +2,12 @@
 namespace PharmaFEFO\Controller\Api;
 
 use PharmaFEFO\Service\StockService;
+
+class ApiDashboardController{
+    private StockService $stockService;
+
+    public function __construct()
+    {
+         $this->stockService= new StockService();
+    }
+}
